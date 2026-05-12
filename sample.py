41bb04ae02c2b68735c318e164eba27ff8ed70e1
Ye Li <charlesliye@gmail.com>
2026-05-12 15:02:27 +0100

    Add a simple greeting program that calculates next year's age

---
+++ sample.py
@@ -0,0 +1,7 @@
+names = input("What is your name? ")
+age = int(input("How old are you? "))
+
+next_year_age = age + 1
+
+print("Hello, " + names + "!")
+print("Next year, you will be", next_year_age, "years old.")
